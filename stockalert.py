7101f2ccf38383fe9c47fb55850b9c5b1b8bfb40
lyle <lyle@pop-os.localdomain>
2020-11-17 13:06:13 -0700

    lunch time work on timing stuff.

--- stockalert.py
+++ stockalert.py
@@ -1,46 +1,73 @@
 import alpaca_trade_api as tradeapi
+import datetime
+import time
 
 api = tradeapi.REST('api key goes here','secret key goes here', base_url='https://api.alpaca.markets')
-account = api.get_account()
-watchListId = 0
+
 companiesLastQuote = {}
 
-#get all my watchlists
-watchLists = api.get_watchlists()
-print(watchLists)
-#only have one so use that one
-watchListId = watchLists[0].id
+def stockChecker():
+    #account = api.get_account()
+    watchListId = 0
+    #get all my watchlists
+    watchLists = api.get_watchlists()
+    print(watchLists)
+    #only have one so use that one
+    watchListId = watchLists[0].id
 
-#get my primary watchList
-watchListCompanies = api.get_watchlist(watchListId)
+    #get my primary watchList
+    watchListCompanies = api.get_watchlist(watchListId)
 
 
-for company in watchListCompanies.assets:
-    companySymbol = company['symbol']
-    lastQuote = api.get_last_quote(companySymbol)
-    currentBid = lastQuote.bidprice
-    #print(lastQuote.bidprice)
-    companyInList = companiesLastQuote.get(companySymbol)
-    
-    if companyInList:
-        previousQuote = companiesLastQuote[companySymbol]
-        if(currentBid > previousQuote):
-            print(f"now might be a good time to sell: {companySymbol} @ {currentBid} price before: {previousQuote}")
-            companiesLastQuote[companySymbol] = currentBid
-        elif(currentBid < previousQuote):
-            print(f"now might be a good time to buy: {companySymbol} @ {currentBid} price before: {previousQuote}")
+    for company in watchListCompanies.assets:
+        companySymbol = company['symbol']
+        lastQuote = api.get_last_quote(companySymbol)
+        currentBid = lastQuote.bidprice
+        #print(lastQuote.bidprice)
+        companyInList = companiesLastQuote.get(companySymbol)
+        
+        if companyInList:
+            previousQuote = companiesLastQuote[companySymbol]
+            if(currentBid > previousQuote):
+                print(f"now might be a good time to sell: {companySymbol} @ {currentBid} price before: {previousQuote}")
+                companiesLastQuote[companySymbol] = currentBid
+            elif(currentBid < previousQuote):
+                print(f"now might be a good time to buy: {companySymbol} @ {currentBid} price before: {previousQuote}")
+            else:
+                companiesLastQuote[companySymbol] = currentBid
         else:
             companiesLastQuote[companySymbol] = currentBid
+
+        # if(len(companiesLastQuote) > 0):
+        #     previousQuote = companiesLastQuote[companySymbol]
+        #     print(previousQuote)
+        # else:
+        #     companiesLastQuote[companySymbol] = lastQuote.bidprice
+
+
+
+def timeInRange():
+    startDate = datetime.datetime.today()
+    
+    #open and closing bell are the times market opens in utah
+    openBell = startDate.replace(hour=7, minute=30)
+    closingBell = startDate.replace(hour=14, minute=00)
+    currentTime = datetime.datetime.today()
+
+    if currentTime >= openBell and currentTime < closingBell:
+        return True
     else:
-        companiesLastQuote[companySymbol] = currentBid
+        return False
 
-    # if(len(companiesLastQuote) > 0):
-    #     previousQuote = companiesLastQuote[companySymbol]
-    #     print(previousQuote)
-    # else:
-    #     companiesLastQuote[companySymbol] = lastQuote.bidprice
 
-print(companiesLastQuote)
+
+tradingIsOpen = timeInRange()
+#print(f'trading is open? {tradingIsOpen}')
+
+while tradingIsOpen:
+    stockChecker()
+    time.sleep(45)
+    #900 seconds = 15 minutes
 
 
 #get the aggs for a company for a specified time frame --
