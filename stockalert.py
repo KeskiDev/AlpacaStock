ef01452fa0087c5e83372649bd4e856ac31db2c4
lyle <keskidev@gmail.com>
2020-11-20 16:58:11 -0700

    more work on changing the message and parameters of when to send message.

--- stockalert.py
+++ stockalert.py
@@ -10,6 +10,8 @@ api = tradeapi.REST('api key','secret key', base_url='https://api.alpaca.markets
 discord_webhook_url = "discord url here"
 
 companiesLastQuote = {}
+companiesOpeningPrices = {}
+#add the high price for the week
 
 def stockChecker():
     #account = api.get_account()
@@ -29,44 +31,58 @@ def stockChecker():
         lastQuote = api.get_last_quote(companySymbol)
         #todo get the barset
         #get_barset(symbol) should give the opening price, top price, close price etc
-        currentBid = lastQuote.bidprice
+
+        currentPrice = lastQuote.askprice
         
         companyInList = companiesLastQuote.get(companySymbol)
+
+        if companiesOpeningPrices.get(companySymbol) is None:
+            companyBar = api.get_barset(companySymbol, 'day', 1)
+            companiesOpeningPrices[companySymbol] = companyBar[companySymbol][0].o
+
+        companyOpeningPrice = companiesOpeningPrices[companySymbol]
         
         if companyInList:
-            previousQuote = companiesLastQuote[companySymbol]
+            #previousQuote = companiesLastQuote[companySymbol]
+            #dayChange = currentPrice - companyOpeningPrice
 
-            if(currentBid > previousQuote):
+            daychange = 23.80-23.85
+            
 
-                msg = "SELL? - {} - ${} - up (+{})"
-                difference = currentBid - previousQuote
-                theDifference = format(difference, '.2f')
+            print(daychange)
 
-                results.append(msg.format(companySymbol, currentBid, theDifference))
-                #print(msg.format(companySymbol, currentBid, difference))
+
+            # if(currentPrice > previousQuote):
+
+            #     msg = "SELL? - {} - ${} - up (+{})"
+            #     difference = currentPrice - previousQuote
+            #     theDifference = format(difference, '.2f')
+
+            #     results.append(msg.format(companySymbol, currentPrice, theDifference))
+            #     #print(msg.format(companySymbol, currentBid, difference))
                 
-                Message = {
-                    "content": msg.format(companySymbol, currentBid, theDifference)
-                }
-                requests.post(discord_webhook_url, data=Message)
-                companiesLastQuote[companySymbol] = currentBid
-            elif(currentBid < previousQuote):
-                msg = "BUY? - {} - ${} - down (-{})"
-                difference = previousQuote - currentBid
-                theDifference = format(difference, '.2f')
+            #     Message = {
+            #         "content": msg.format(companySymbol, currentPrice, theDifference)
+            #     }
+            #     requests.post(discord_webhook_url, data=Message)
+            #     companiesLastQuote[companySymbol] = currentPrice
+            # elif(currentPrice < previousQuote):
+            #     msg = "BUY? - {} - ${} - down (-{})"
+            #     difference = previousQuote - currentPrice
+            #     theDifference = format(difference, '.2f')
                 
-                results.append(msg.format(companySymbol, currentBid, theDifference))
+            #     results.append(msg.format(companySymbol, currentPrice, theDifference))
 
-                Message = {
-                    "content": msg.format(companySymbol, currentBid, theDifference)
-                }
-                requests.post(discord_webhook_url, data=Message)
+            #     Message = {
+            #         "content": msg.format(companySymbol, currentPrice, theDifference)
+            #     }
+            #     requests.post(discord_webhook_url, data=Message)
 
-                companiesLastQuote[companySymbol] = currentBid
-            else:
-                companiesLastQuote[companySymbol] = currentBid
+            #     companiesLastQuote[companySymbol] = currentPrice
+            # else:
+            #     companiesLastQuote[companySymbol] = currentPrice
         else:
-            companiesLastQuote[companySymbol] = currentBid
+            companiesLastQuote[companySymbol] = currentPrice
     
     
 
@@ -76,24 +92,38 @@ def stockChecker():
 def runAnalysis():
     count = 0
 
-    while True:
-        market = api.get_clock()
-        print(count)
-        count += 1
-        if market.is_open:
-            stockChecker()
-            time.sleep(300)
-            #900 seconds = 15 minutes
-            #1800 seconds = 30 minutes
-            #3600 seconds = 1 hour
-        else:
-            #wait an hour before checking again
-            #check when the market opens
-            secondsToNextOpen = market.next_open - market.timestamp
-            print(f"{market.next_open} - in {secondsToNextOpen} seconds")
+
+    apple = api.get_last_quote('AAPL')
+
+    appleBarset = api.get_barset('AAPL', 'day', 1)
+
+    daychange = apple.askprice - appleBarset['AAPL'][0].o
+
+    print(f"ask = {apple.askprice} : opening = {appleBarset['AAPL'][0].o} :: day change {daychange}")
+            
+    if abs(daychange) > 1.00:
+        print("more than $1 change")
+    else:
+        print("you didn't see anything")
+
+    # while True:
+    #     market = api.get_clock()
+    #     print(count)
+    #     count += 1
+    #     if market.is_open:
+    #         stockChecker()
+    #         time.sleep(300)
+    #         #900 seconds = 15 minutes
+    #         #1800 seconds = 30 minutes
+    #         #3600 seconds = 1 hour
+    #     else:
+    #         #wait an hour before checking again
+    #         #check when the market opens
+    #         secondsToNextOpen = market.next_open - market.timestamp
+    #         print(f"{market.next_open} - in {secondsToNextOpen} seconds")
 
 
-            time.sleep(secondsToNextOpen.total_seconds())
+    #         time.sleep(secondsToNextOpen.total_seconds())
 
 
     #get the aggs for a company for a specified time frame --
